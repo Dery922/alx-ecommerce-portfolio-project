14125d8ff46d4f3a93317ee12e73f4b6e28ca05a
mada <mada@localhost.localdomain>
2024-07-10 00:29:41 +0000

    initail commit

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -3,12 +3,10 @@ import { Link } from "react-router-dom";
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from "../components/Rating";
 import products from "../products";
-import { FaItalic, FaProductHunt} from "react-icons/fa";
+import {  FaProductHunt} from "react-icons/fa";
 
 const ProductScreen = () => {
     const {id:productId} = useParams();
-    let data = useParams()
-    console.log( data = useParams())
     const product = products.find((p) => p._id === productId)
 
      return (
